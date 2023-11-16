432f5cfbb40e367d8e001412881f5f06f9ed2abd
panditdhamdhere <dhamdherepandit@gmail.com>
2023-11-16 14:02:59 +0530

    transaction status compo updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8338e22ba8da4cf5"; }
+/******/ 		__webpack_require__.h = function() { return "a92d7672f88681ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
