5c0b71dd24cf2278cc1f598d780cf41a466c02dd
panditdhamdhere <dhamdherepandit@gmail.com>
2023-11-20 07:48:29 +0530

    swap field done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1466c2869ce6d6c7"; }
+/******/ 		__webpack_require__.h = function() { return "a89f2a3f5fc5b621"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/SwapField.js
+++ components/SwapField.js
@@ -1,7 +1,35 @@
 import React from "react";
+import Selector from "./Selector";
 
-const SwapField = () => {
-  return <div>SwapField</div>;
-};
+const SwapField = React.forwardRef(({ obj }, inputRef) => {
+  const { id, value = "", setValue, defaultValue, ignoreValue, setToken } = obj;
+
+  return (
+    <div className="flex items-center rounded-xl">
+      <input
+        ref={inputRef}
+        className={getInputClassname()}
+        type="number"
+        value={value}
+        placeholder={"0.0"}
+        onChange={(e) => setValue(e.target.value)}
+      />
+
+      <Selector
+        id={id}
+        setToken={setToken}
+        defaultValue={defaultValue}
+        ignoreValue={ignoreValue}
+        // value={value}
+      />
+    </div>
+  );
+
+  function getInputClassname() {
+    let className =
+      "w-full outline-none h-8 px-2 appearance-none text-3xl bg-transparent ";
+    return className;
+  }
+});
 
 export default SwapField;
